7febeb65e9568f5f6e968e0f50d07e350afc10ef
avinashd <d.nash.avi@gmail.com>
2014-11-28 22:02:54 +0000

    Make DSL work

--- lib/redd/dsl.rb
+++ lib/redd/dsl.rb
@@ -15,40 +15,40 @@ module Redd
   #     end
   #   end
   module DSL
-    class << self
-      # Create or access the client.
-      # @return [Redd::Clients::Client] The client.
-      def client
-        @client ||= Redd::Clients::Unauthenticated.new
-      end
+    private
 
-      # Login with the username and password and replace the client with the
-      # logged in one.
-      #
-      # @param [String] username
-      # @param [String] password
-      # @return [Redd::Clients::Client] The logged-in client.
-      def login(username, password)
-        @client = Redd::Clients::Unauthenticated.new(username, password)
-      end
+    # Create or access the client.
+    # @return [Redd::Clients::Client] The client.
+    def client
+      @client ||= Redd::Clients::Unauthenticated.new
+    end
 
-      # @return [Boolean] If the current user is logged in.
-      def logged_in?
-        !@client.is_a?(Redd::Clients::Unauthenticted)
-      end
+    # Login with the username and password and replace the client with the
+    # logged in one.
+    #
+    # @param [String] username
+    # @param [String] password
+    # @return [Redd::Clients::Client] The logged-in client.
+    def login(username, password)
+      @client = Redd::Clients::Unauthenticated.new(username, password)
+    end
 
-      # Reset the client back to the unauthenticated one.
-      # @return [Redd::Clients::Client] The unauthenticated client.
-      def logout!
-        @client = Redd::Clients::Unauthenticated.new
-      end
-      alias_method :reset_client!, :logout!
+    # @return [Boolean] If the current user is logged in.
+    def logged_in?
+      !@client.is_a?(Redd::Clients::Unauthenticted)
+    end
 
-      # @see http://git.io/KNmwIw
-      def method_missing(meth, *args, &block)
-        return super unless @client.respond_to?(meth)
-        @client.send(meth, *args, &block)
-      end
+    # Reset the client back to the unauthenticated one.
+    # @return [Redd::Clients::Client] The unauthenticated client.
+    def logout!
+      @client = Redd::Clients::Unauthenticated.new
+    end
+    alias_method :reset_client!, :logout!
+
+    # @see http://git.io/KNmwIw
+    def method_missing(meth, *args, &block)
+      return super unless @client.respond_to?(meth)
+      @client.send(meth, *args, &block)
     end
   end
 end
