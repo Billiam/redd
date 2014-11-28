c7818540a06ca19a23b6fdceff37d3fa2192bd03
avinashd <d.nash.avi@gmail.com>
2014-11-28 12:23:47 +0000

    Add Faraday middleware

---
+++ lib/redd/response/parse_json.rb
@@ -0,0 +1,19 @@
+require "faraday_middleware/response_middleware"
+
+module Redd
+  # The module that contains middleware that alters the Faraday response.
+  module Response
+    # Faraday Middleware that parses JSON using OJ, via MultiJson.
+    class ParseJson < FaradayMiddleware::ResponseMiddleware
+      dependency "multi_json"
+
+      define_parser do |body|
+        MultiJson.load(body, symbolize_keys: true)
+      end
+
+      def parse_response?(env)
+        response_type(env).include?("json")
+      end
+    end
+  end
+end
---
+++ lib/redd/response/raise_error.rb
@@ -0,0 +1,17 @@
+require "faraday_middleware/response_middleware"
+
+module Redd
+  # The module that contains middleware that alters the Faraday response.
+  module Response
+    # Faraday Middleware that raises an error if there's one.
+    class RaiseError < FaradayMiddleware::ResponseMiddleware
+      def parse_response?(env)
+        @error = Redd::Error.from_response(env)
+      end
+
+      def process_response(_)
+        fail @error if @error
+      end
+    end
+  end
+end
