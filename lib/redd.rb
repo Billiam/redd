49575ddd8f46c9c5cb6451b43516ddbbb86b13af
avinashd <d.nash.avi@gmail.com>
2014-11-29 13:40:13 +0000

    Add Redd.new

--- lib/redd.rb
+++ lib/redd.rb
@@ -1,5 +1,14 @@
-require "redd/version"
+require_relative "redd/version"
+require_relative "redd/clients/authenticated"
+require_relative "redd/clients/unauthenticated"
 
 # The main Redd module.
 module Redd
+  def self.new(username: nil, password: nil)
+    if username && password
+      Redd::Clients::Authenticated.new(username, password)
+    else
+      Redd::Clients::Unauthenticated.new
+    end
+  end
 end
