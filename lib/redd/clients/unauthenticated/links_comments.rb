70b1c3b131b07d8ce3806acc764f02f2681e2d4a
avinashd <d.nash.avi@gmail.com>
2014-11-30 15:44:25 +0000

    Improve get_info

--- lib/redd/clients/unauthenticated/links_comments.rb
+++ lib/redd/clients/unauthenticated/links_comments.rb
@@ -2,17 +2,24 @@ module Redd
   module Clients
     class Unauthenticated
       module LinksComments
-        # @param params [Hash] A hash of parameters to send to reddit.
-        # @option params [String] :id The fullname of a thing.
-        # @option params [String] :url The url of a thing. If an id is also
+        # @option [Array<String>] id An array of fullnames.
+        # @option [String] url The url of a thing. If an id is also
         #   provided, the id will take precedence.
         # @return [Redd::Objects::Listing] Listing of the object or objects.
         #
         # @note Reddit does accept a subreddit, but with fullnames and urls, I
         #   assumed that was unnecessary.
-        def get_info(params = {})
+        def get_info(id: [], url: "")
+          if !id.empty?
+            params = {id: id.join(",")}
+          else
+            params = {url: url}
+          end
+           
           object_from_response :get, "/api/info.json", params
         end
+        alias_method :get_info, :comment
+        alias_method :get_info, :submission
       end
     end
   end
