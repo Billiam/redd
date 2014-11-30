0492bb2f0127004247c575b612020d38ab6dc2ff
Avinash Dwarapu <d.nash.avi@gmail.com>
2014-11-30 23:47:43 +0100

    Change new method to it

--- lib/redd.rb
+++ lib/redd.rb
@@ -3,14 +3,11 @@ require_relative "redd/clients/unauthenticated"
 
 # The main Redd module.
 module Redd
-  class << self
-    def new(username = nil, password = nil, **kwargs)
-      if username && password
-        Redd::Clients::Unauthenticated.new.login(username, password, **kwargs)
-      else
-        Redd::Clients::Unauthenticated.new(**kwargs)
-      end
+  def self.it(username = nil, password = nil, **kwargs)
+    if username && password
+      Redd::Clients::Unauthenticated.new.login(username, password, **kwargs)
+    else
+      Redd::Clients::Unauthenticated.new(**kwargs)
     end
-    alias_method :it, :new
   end
 end
